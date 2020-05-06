d8dd292d53581a96b441959efc2c0d81b9967e1d
Alexis Navarrete Puebla <42220915+PredadorAkrid@users.noreply.github.com>
2020-05-06 02:31:31 -0500

    Update forms.py

--- proyecto/cc/forms.py
+++ proyecto/cc/forms.py
@@ -2,10 +2,10 @@ from django import forms
 from .models import Post
 import datetime
 from django.contrib.admin.widgets import AdminTimeWidget
-
+import django
 class PostForm(forms.ModelForm):
     title = forms.CharField(label="Title", max_length=250, required=True)
-    #date = forms.DateField(initial=datetime.date.today, widget=AdminTimeWidget(format=' %H:%M:%S'));
+    #date = forms.DateField(disabled =True, default=django.utils.timezone.now);
     paragraph = forms.CharField(label="Paragraph", max_length=250, required=False)
     image = forms.FileField(label="Image", required=False)
     
@@ -16,5 +16,10 @@ class PostForm(forms.ModelForm):
         fields = ("title",  "date","paragraph", "image")
     def __init__(self, *args, **kwargs):
         super(PostForm, self).__init__(*args, **kwargs)
-        #self.fields['date'].widget.attrs['disabled'] = 'disabled'
-        
+        self.fields['date'].widget.attrs['disabled'] = 'disabled'
+        self.fields['date'].required = False
+    def clean_type(self):
+        if self.instance and self.instance.pk:
+            return self.instance.date
+        else:
+            return self.cleaned_data['date']
