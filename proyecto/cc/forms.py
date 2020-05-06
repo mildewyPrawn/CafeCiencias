38f03d2e858a5994a7890ad164efc3ca4fb859c2
Alexis Navarrete <alexisnpfc@ciencias.unam.mx>
2020-05-06 01:17:44 -0500

    form 02

--- proyecto/cc/forms.py
+++ proyecto/cc/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 from .models import Post
 import datetime
+from django.contrib.admin.widgets import AdminTimeWidget
 
 class PostForm(forms.ModelForm):
     title = forms.CharField(label="Title", max_length=250, required=True)
-    date = forms.DateField(initial=datetime.date.today);
+    date = forms.DateField(initial=datetime.date.today, widget=AdminTimeWidget(format=' %H:%M:%S'));
     paragraph = forms.CharField(label="Paragraph", max_length=250, required=False)
     image = forms.FileField(label="Image", required=False)
     
